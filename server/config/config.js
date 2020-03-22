38514e4929658a6be44fabe27a10297a2019fb48
Fede <federico@kumelen.studio>
2020-03-22 03:08:07 -0300

    Varialbe de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb+srv://maserati:4g3YtBGPXTSpLe7@cluster0-i5ds7.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
