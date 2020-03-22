724bae1092fa088a988754fd855a782ae79f79d3
Fede <federico@kumelen.studio>
2020-03-22 02:35:24 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 // ============
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ============
+// Entorno
+// ============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============
+// bbdd
+// ============
+
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb+srv://maserati:4g3YtBGPXTSpLe7@cluster0-i5ds7.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
